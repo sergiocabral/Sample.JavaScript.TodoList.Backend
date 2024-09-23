08c184b4ed7353b24bfef30ca83c98e3f336c356
Sergio Cabral <git@sergiocabral.com>
2024-09-22 22:05:29 -0300

    Rota para ler todas as tarefas

--- src/aplicativo.js
+++ src/aplicativo.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { lerTarefas, gravarTarefas } from './tarefas.js'
 
 const app = express()
 
@@ -7,7 +8,16 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (request, response) => response.send('Olá Tarefas'))
-app.get('/tarefas', (request, response) => response.send('Não implementado'))
+
+app.get('/tarefas', async (request, response) => {
+  try {
+    const tarefas = await lerTarefas()
+    response.json(tarefas)
+  } catch (error) {
+    response.status(500).json({ error: 'Erro ao obter tarefas' })
+  }
+})
+
 app.get('/tarefa/:id', (request, response) => response.send('Não implementado'))
 app.post('/tarefa', (request, response) => response.send('Não implementado'))
 app.put('/tarefa/:id', (request, response) => response.send('Não implementado'))
