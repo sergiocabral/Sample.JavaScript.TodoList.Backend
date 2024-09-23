8f9d1d6f3e3507f9693a4fdce487bb03aac58f71
Sergio Cabral <git@sergiocabral.com>
2024-09-22 21:46:20 -0300

    Planejamento de rotas da aplicação

--- src/aplicativo.js
+++ src/aplicativo.js
@@ -6,6 +6,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (request, response) => response.send('Olá Tarefas'))
+app.get('/tarefas', (request, response) => response.send('Não implementado'))
+app.get('/tarefa/:id', (request, response) => response.send('Não implementado'))
+app.post('/tarefa', (request, response) => response.send('Não implementado'))
+app.put('/tarefa/:id', (request, response) => response.send('Não implementado'))
+app.delete('/tarefa/:id', (request, response) => response.send('Não implementado'))
+app.path('/tarefa/:id/completa', (request, response) => response.send('Não implementado'))
+app.path('/tarefa/:id/incompleta', (request, response) => response.send('Não implementado'))
+
 function iniciar(port) {
   app.listen(port, () => console.log(`Aplicação executando em http://localhost:${port}/`))
 }
